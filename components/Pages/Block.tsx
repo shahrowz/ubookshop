cdf306a43456e800d596b4ddd277b6cd608e3157
shahrowz <$shahrowzdivdel@gmail.com>
2024-09-06 18:51:41 +0330

    Update

---
+++ components/Pages/Block.tsx
@@ -0,0 +1,32 @@
+export const Block = props => {
+
+    if (!props.state.faves) {
+        props.state.faves = []
+    }
+
+
+
+    return <f-x style={{
+        width: 150, height: 200, flex: 1, minWidth: 150,
+        position: "relative"
+    }}>
+
+        <img
+            className={global.styles.hover}
+            src={props.book.imageLink}
+            style={{ width: 150, height: 200, objectFit: "fill", flex: 1, minWidth: 150 }}
+            onClick={() => {
+                props.state.form = "bookspecs"
+                props.state.book = props.book
+                props.refresh()
+            }} />
+
+        {props.state.faves.includes(props.book.title) ?
+            <img src="https://cdn.ituring.ir/research/39/ubookshop/heart-rate-icon.png"
+                style={{
+                    height: 30, width: 30, objectFit: "contain",
+                    position: "absolute", bottom: 10, right: 10
+                }} /> : null}
+
+    </f-x>
+}
