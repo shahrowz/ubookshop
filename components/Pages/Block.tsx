0a571d8cf73cae9da26915a64e0b800e91a281ca
shahrowz <$shahrowzdivdel@gmail.com>
2024-09-08 04:32:41 +0330

    Update

--- components/Pages/Block.tsx
+++ components/Pages/Block.tsx
@@ -1,32 +1,59 @@
 export const Block = props => {
 
-    if (!props.state.faves) {
-        props.state.faves = []
-    }
 
 
+    let size = props.state.cart.includes(props.book.title) ? 20 : 25
 
-    return <f-x style={{
-        width: 150, height: 200, flex: 1, minWidth: 150,
-        position: "relative"
+    return <c-c style={{
+        width: 150, flex: 1, minWidth: 150,
+        position: "relative", backgroundColor: "white",
+        borderRadius: 5, margin: "10px",
+        boxShadow: " 0px 0px 9px 2px rgba(0,0,0,0.60)"
     }}>
 
         <img
             className={global.styles.hover}
             src={props.book.imageLink}
-            style={{ width: 150, height: 200, objectFit: "fill", flex: 1, minWidth: 150 }}
+            style={{
+                width: "100%",
+                height: 200,
+                objectFit: "fill",
+                minWidth: 150,
+                borderTopLeftRadius: 5,
+                borderTopRightRadius: 5
+            }}
             onClick={() => {
                 props.state.form = "bookspecs"
                 props.state.book = props.book
                 props.refresh()
             }} />
 
-        {props.state.faves.includes(props.book.title) ?
-            <img src="https://cdn.ituring.ir/research/39/ubookshop/heart-rate-icon.png"
-                style={{
-                    height: 30, width: 30, objectFit: "contain",
-                    position: "absolute", bottom: 10, right: 10
-                }} /> : null}
+        <f-cc style={{
+            padding: "5px 8px", width: "100%", height: 40,
+            direction: "ltr"
+        }}>
+            <f-12>
+                {props.book.title}
+            </f-12>
+        </f-cc>
 
-    </f-x>
+        <hr style={{ width: "85%", opacity: 0.4 }} />
+
+        <f-csb style={{ width: "100%", padding: "5px 0" }}>
+
+            <img src={props.state.cart.includes(props.book.title) ?
+                "https://cdn.ituring.ir/qepal/ok.svg" :
+                "https://cdn.ituring.ir/qepal/cart.svg"}
+                style={{
+                    height: 25, width: 25,
+                    objectFit: "contain", margin: "0 10px"
+                }} />
+
+            <c-x style={{ direction: "ltr", margin: "0 10px" }}>
+                <f-12><del>{(props.book.price as number).toLocaleString("fa-IR")} تومان</del></f-12>
+                <f-15>{(props.book.price * 0.9 as number).toLocaleString("fa-IR")} تومان</f-15>
+            </c-x>
+        </f-csb>
+
+    </c-c>
 }
--- components/Pages/index.tsx
+++ components/Pages/index.tsx
@@ -14,90 +14,142 @@ import { Block } from './Block';
 
 
 export default p => Component(p, Page);
-const Page: PageEl = (props, state, refresh, getProps) => {
+const Page: PageEl = (props, state:
+  {
+    form: string,
+    book: {
+      title: string, author: string, country: string,
+      imageLink: string, price: number,
+      language: string, pages: number,
+
+    },
+    cart: Array<string>
+  }, refresh, getProps) => {
 
   let styles = global.styles
-  let name = "خوش آمدید"
 
+  let total_price = 0
+
+
+  if (!state.cart) {
+    state.cart = []
+  }
+
+
+  for (let title of state.cart) {
+    let book = props.books.find(b => b.title == title)
+    if (book) {
+      total_price += (book.price * 0.9)
+    }
+  }
 
   return (
     <div style={{ direction: "rtl", minHeight: "11vh", }}>
       <br-x />
 
-      {state.form == "bookspecs" ? <WindowFloat title="مشخصات کتاب" onclose={() => {
-        delete state.form
-        refresh()
-      }}>
-        {/* <pre>{JSON.stringify(state.book, null, 2)}</pre> */}
-
-        <f-c>
-          <f-15>
-            نام کتاب :
-          </f-15>
-          <sp-2 />
-          <f-15>
-            {state.book.title}
-          </f-15>
-        </f-c>
-        <f-c>
-          <f-15>
-            نویسنده :
-          </f-15>
-          <sp-2 />
-          <f-15>
-            {state.book.author}
-          </f-15>
-        </f-c>
-        <f-c>
-          <f-15>
-            کشور سازنده:
-          </f-15>
-          <sp-2 />
-          <f-15>
-            {state.book.country}
-          </f-15>
-        </f-c>
-        <f-c>
-          <f-15>
-            زبان کتاب :
-          </f-15>
-          <sp-2 />
-          <f-15>
-            {state.book.language}
-          </f-15>
-        </f-c><f-c>
-          <f-15>
-            تعداد صفحات :
-          </f-15>
-          <sp-2 />
-          <f-15>
-            {(state.book.pages as number).toLocaleString("fa-IR")}
-          </f-15>
-        </f-c>
-
-
-        <g-b style={{ backgroundColor: "BlueViolet" }} onClick={() => {
-          if (!state.faves) {
-            state.faves = []
-          }
-          state.faves.push(state.book.title)
-          state.form = null
+      {state.form == "bookspecs" ?
+        <WindowFloat title="مشخصات کتاب" onclose={() => {
+          delete state.form
           refresh()
         }}>
+          {/* <pre>{JSON.stringify(state.book, null, 2)}</pre> */}
 
-          <img src="https://cdn.ituring.ir/research/39/ubookshop/heart-rate-icon.png"
-            style={{ height: 20, width: 20, objectFit: "contain" }} />
+          <f-c>
+            <f-15>
+              نام کتاب :
+            </f-15>
+            <sp-2 />
+            <f-15>
+              {state.book.title}
+            </f-15>
+          </f-c>
+          <f-c>
+            <f-15>
+              نویسنده :
+            </f-15>
+            <sp-2 />
+            <f-15>
+              {state.book.author}
+            </f-15>
+          </f-c>
+          <f-c>
+            <f-15>
+              کشور سازنده:
+            </f-15>
+            <sp-2 />
+            <f-15>
+              {state.book.country}
+            </f-15>
+          </f-c>
+          <f-c>
+            <f-15>
+              زبان کتاب :
+            </f-15>
+            <sp-2 />
+            <f-15>
+              {state.book.language}
+            </f-15>
+          </f-c><f-c>
+            <f-15>
+              تعداد صفحات :
+            </f-15>
+            <sp-2 />
+            <f-15>
+              {(state.book.pages as number).toLocaleString("fa-IR")}
+            </f-15>
+          </f-c>
 
-        </g-b>
+
+          <g-b style={{
+            backgroundColor:
+              state.cart.includes(state.book.title) ? "firebrick" : "darkseagreen"
+          }}
+            onClick={() => {
+
+              if (state.cart.includes(state.book.title)) {
+
+                state.cart = state.cart.filter(bookname => state.book.title != bookname)
+                state.form = null
+                refresh()
+
+              }
+              else {
+
+                state.cart.push(state.book.title)
+                state.form = null
+                refresh()
+
+              }
+
+            }}>
+
+            {state.cart.includes(state.book.title) ? <f-13>حذف از سبد خرید</f-13> : <f-13>افزودن به سبد خرید</f-13>}
+
+          </g-b>
 
 
 
 
 
 
-      </WindowFloat> : null}
+        </WindowFloat> : null}
 
-      <Window title={name}
+
+
+
+
+      <Window title="سبد خرید" style={{
+        margin: 10,
+        width: "calc(100% - 20px)"
+      }}>
+        <f-cse style={{
+          height: 60, width: "100%"
+        }}>
+          <f-14>مجموع قابل پرداخت : {total_price.toLocaleString("fa-IR")} تومان </f-14>
+          <f-14>تعداد کتاب ها : {state.cart.length.toLocaleString("fa-IR")}</f-14>
+        </f-cse>
+      </Window>
+      <Window title={"خوش آمدید"}
         style={{ minHeight: 200, margin: 10, width: "calc(100% - 20px)" }}>
         {/* <pre style={{ direction: "ltr" }}>{JSON.stringify(props, null, 2)}</pre> */}
 
